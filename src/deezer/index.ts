3be4e43519412f70e194325a2d3b006fd0cf933e
Rodolfo Vieira <rodolfovieira95@outlook.com>
2024-04-14 02:55:36 -0300

    feat: exports added functions

--- src/deezer/index.ts
+++ src/deezer/index.ts
@@ -1,1 +1,7 @@
+export { getAlbumInfo } from "./album";
+export { getArtistInfo } from "./artist";
+export { getChartInfo } from "./chart";
+export { getGenreInfo } from "./genre";
+export { getPlaylistInfo } from "./playlist";
 export { search } from "./search";
+export { getTrackInfo } from "./track";
