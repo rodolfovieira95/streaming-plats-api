4b1cc485fb070107685cc2392b793047affa2913
Rodolfo Vieira <rodolfovieira95@outlook.com>
2024-04-19 23:07:04 -0300

    feat: adds addPlaylistTracks and deletePlaylistTracks

--- src/deezer/playlist.test.ts
+++ src/deezer/playlist.test.ts
@@ -1,6 +1,13 @@
-import { getPlaylistInfo } from "./playlist";
+import {
+  addPlaylistTracks,
+  deletePlaylistTracks,
+  getPlaylistInfo,
+} from "./playlist";
+import { createPlaylist, deletePlaylist } from "./user";
 
+const yourUserAccessToken = process.env.ACCESS_TOKEN_TEST || "";
 const randomPlaylistId = "908622995";
+const tracklistIds = ["129313094", "140856305", "2421997365", "2635513182"];
 
 test("Tests getPlaylistInfo with random playlist ID", async () => {
   const { id, type } = await getPlaylistInfo(randomPlaylistId);
@@ -14,4 +21,29 @@ test("Tests getPlaylistInfo with empty ID", async () => {
   expect(error?.code).toBe(600);
   expect(error?.message).toBe("Unknown path components : /playlist");
   expect(error?.type).toBe("InvalidQueryException");
+});
+
+test("Tests creating a playlist and adds 4 tracks to it, deletes 2 tracks and delete the playlist.", async () => {
+  const creationResult = await createPlaylist(
+    yourUserAccessToken,
+    "Test Playlist",
+  );
+  expect(creationResult).not.toBe(null || undefined);
+  const addTracksResult = await addPlaylistTracks(
+    creationResult.id,
+    yourUserAccessToken,
+    tracklistIds,
+  );
+  expect(addTracksResult).toBe(true);
+  const deleteTracks = await deletePlaylistTracks(
+    creationResult.id,
+    yourUserAccessToken,
+    ["140856305", "2421997365"],
+  );
+  expect(deleteTracks).toBe(true);
+  const deleteResult = await deletePlaylist(
+    yourUserAccessToken,
+    creationResult.id,
+  );
+  expect(deleteResult).toBe(true);
 });
--- src/deezer/playlist.ts
+++ src/deezer/playlist.ts
@@ -10,3 +10,35 @@ export const getPlaylistInfo = async (
   const result = await fetch(`${DEEZER_API_URL}/playlist/${id}`);
   return await result.json();
 };
+
+export const addPlaylistTracks = async (
+  id: string,
+  accessToken: string,
+  trackIds: string[],
+): Promise<
+  DeezerPlaylistType & {
+    error?: DeezerResultErrorType;
+  }
+> => {
+  const result = await fetch(
+    `${DEEZER_API_URL}/playlist/${id}/tracks?access_token=${accessToken}&songs=${trackIds.toString()}`,
+    { method: "POST" },
+  );
+  return await result.json();
+};
+
+export const deletePlaylistTracks = async (
+  id: string,
+  accessToken: string,
+  trackIds: string[],
+): Promise<
+  DeezerPlaylistType & {
+    error?: DeezerResultErrorType;
+  }
+> => {
+  const result = await fetch(
+    `${DEEZER_API_URL}/playlist/${id}/tracks?access_token=${accessToken}&songs=${trackIds.toString()}`,
+    { method: "DELETE" },
+  );
+  return await result.json();
+};
